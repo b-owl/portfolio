1e4f36e8b6c513ac7b13a3967f7cd82b85ae9812
Mhdi <0MehdiKeramati1@gmail.com>
2024-06-26 13:18:50 +0330

    initial project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  //   import Counter from "./Counter.svelte";
+  //   import Counter from "./Counter.svelte";
+</script>
+
+<svelte:head>
+  <title>Mehdi Keramti</title>
+  <meta name="description" content="Mehdi keramati portfolio" />
+</svelte:head>
+
+<section>
+  <h1>
+    to your new<br />SvelteKit app
+  </h1>
+
+  <h2>
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
+
+  h1 {
+    width: 100%;
+  }
+</style>
